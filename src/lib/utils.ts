9757f1f5440b041d26ae9aa91e3da1a6156c7c5a
Javier Diaz <coderdiaz@gmail.com>
2024-07-12 00:29:04 -0600

    feat: added constants and base utils

---
+++ src/lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { constants } from './constants';
+
+/**
+ * Merges class names using tailwind-merge and clsx
+ * @param inputs A list of class names to be merged
+ * @returns The merged class names
+ */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export const generateAbsoluteUrl = (path: string) => constants.BASE_URL.concat(path);
