3359b5fa13bdc6f3e7ca847ef22a54d0065f97cc
Javier Diaz <coderdiaz@gmail.com>
2024-07-12 00:28:44 -0600

    feat: configured fonts in tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,10 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+      sans: ['ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"','"Segoe UI Emoji"', 'Segoe UI Symbol','"Noto Color Emoji"'],
+      serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+    },
 		extend: {},
 	},
 	plugins: [],
