70311214cf6a31b8b4e502a76fa2b5e282396c87
Javier Diaz <coderdiaz@gmail.com>
2025-01-16 12:38:11 -0600

    feat: enabled new article schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,13 @@
 import { glob } from 'astro/loaders';
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z, type ImageFunction } from 'astro:content';
+
+const seoSchema = (image: ImageFunction) => z.object({
+  title: z.string(),
+  description: z.string(),
+  image: image().optional(),
+  keywords: z.array(z.string()).optional(),
+  canonicalUrl: z.string(),
+})
 
 const modules = defineCollection({
   loader: glob({ pattern: '**/*.md', base: './src/content/modules' }),
@@ -12,6 +20,17 @@ const modules = defineCollection({
       url: z.string(),
     })
   })
+});
+
+const articles = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.{md,mdx}', base: './src/content/articles' }),
+  schema: ({ image }) => z.object({
+    draft: z.boolean(),
+    title: z.string(),
+    featuredImage: image().optional(),
+    publishedAt: z.date().nullable(),
+    seo: seoSchema(image).optional()
+  })
 })
 
-export const collections = { modules };
+export const collections = { modules, articles };
