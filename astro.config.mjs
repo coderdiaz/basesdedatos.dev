6eebb27b4814d9a7d53b80240f44a05c4a1b91ae
Javier Diaz <coderdiaz@gmail.com>
2025-04-15 20:30:20 -0600

    chore: updated site info

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://basesdedatos.dev/',
+  trailingSlash: 'always',
   integrations: [react(), tailwind()]
 });
